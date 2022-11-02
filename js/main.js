de5c8ad945ea07d128045a306896a0bebe6e279f
“vicky” <s6102161021@gmail.com>
2022-11-02 10:40:56 +0800

    新增：新增表單功能

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ $(function () {
   });
 });
 
-const data = [
+let data = [
   {
     id: 0,
     name: '肥宅心碎賞櫻3日',
@@ -51,3 +51,130 @@ const data = [
     rate: 7,
   },
 ];
+
+let _list = document.querySelector('.dataList__card');
+let count = document.getElementById('count');
+const dataList = document.getElementById('dataList');
+
+function init() {
+  let str = '';
+  let countNum = 0;
+  data.forEach(function (item) {
+    countNum+=1;
+    let content = `<li class="card">
+            <p class="card__area">${item.area}</p>
+            <div class="card__photo">
+              <img src="${item.imgUrl}" alt="" />
+            </div>
+            <div class="card__info">
+              <p class="card__info__star">${item.rate}</p>
+              <h3 class="card__info__title">${item.name}</h3>
+              <p class="card__info__describe">
+                ${item.description}
+              </p>
+              <div class="card__info__price">
+                <p>
+                  <i class="fa-solid fa-circle-exclamation"></i>剩下最後 ${item.group} 組
+                </p>
+                <div class="price">
+                  <p>TWD</p>
+                  <span>$${item.price}</span>
+                </div>
+              </div>
+            </div>
+          </li>`;
+    str += content;
+    _list.innerHTML = str;
+  });
+  count.textContent = countNum;
+}
+init();
+
+function showList(e) {
+  let select = document.getElementById('dataList').value;
+  let countNum = 0;
+
+  let str = '';
+  data.forEach(function (item) {
+    if (select === item.area) {
+      countNum+=1;
+      let content = `<li class="card">
+            <p class="card__area">${item.area}</p>
+            <div class="card__photo">
+              <img src="${item.imgUrl}" alt="" />
+            </div>
+            <div class="card__info">
+              <p class="card__info__star">${item.rate}</p>
+              <h3 class="card__info__title">${item.name}</h3>
+              <p class="card__info__describe">
+                ${item.description}
+              </p>
+              <div class="card__info__price">
+                <p>
+                  <i class="fa-solid fa-circle-exclamation"></i>剩下最後 ${item.group} 組
+                </p>
+                <div class="price">
+                  <p>TWD</p>
+                  <span>$${item.price}</span>
+                </div>
+              </div>
+            </div>
+          </li>`;
+      str += content;
+    } else if(select === '地區搜尋'){
+      countNum += 1;
+      let content = `<li class="card">
+            <p class="card__area">${item.area}</p>
+            <div class="card__photo">
+              <img src="${item.imgUrl}" alt="" />
+            </div>
+            <div class="card__info">
+              <p class="card__info__star">${item.rate}</p>
+              <h3 class="card__info__title">${item.name}</h3>
+              <p class="card__info__describe">
+                ${item.description}
+              </p>
+              <div class="card__info__price">
+                <p>
+                  <i class="fa-solid fa-circle-exclamation"></i>剩下最後 ${item.group} 組
+                </p>
+                <div class="price">
+                  <p>TWD</p>
+                  <span>$${item.price}</span>
+                </div>
+              </div>
+            </div>
+          </li>`;
+      str += content;
+    }
+    _list.innerHTML = str;
+  });
+  count.textContent = countNum;
+
+}
+dataList.addEventListener('change', showList);
+
+let _add = document.querySelector('.selectCard__button');
+_add.addEventListener('click',function(){
+  let name = document.getElementById('itemName').value;
+  let imgUrl = document.getElementById('itemImg').value;
+  let area = document.getElementById('itemArea').value;
+  let description = document.getElementById('itemDescription').value;
+  let group = document.getElementById('itemGroup').value;
+  let price = document.getElementById('itemPrice').value;
+  let rate = document.getElementById('itemRate').value;
+  let newData = {
+    id: 2,
+    name,
+    imgUrl,
+    area,
+    description,
+    group,
+    price,
+    rate,
+  };
+  data.push(newData);
+  init();
+})
+
+// console.log(data);
