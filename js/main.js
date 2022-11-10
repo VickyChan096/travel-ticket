bf1eb5ea179d9ee1dbe4dc7cc30f0bf73a0584b3
“vicky” <s6102161021@gmail.com>
2022-11-10 15:21:49 +0800

    修改：axios data/ sort data

--- js/main.js
+++ js/main.js
@@ -1,58 +1,42 @@
-const data = [
-  {
-    id: 0,
-    name: '肥宅心碎賞櫻3日',
-    imgUrl:
-      'https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80',
-    area: '高雄',
-    description: '賞櫻花最佳去處。肥宅不得不去的超讚景點！',
-    group: 87,
-    price: 1400,
-    rate: 10,
-  },
-  {
-    id: 1,
-    name: '貓空纜車雙程票',
-    imgUrl:
-      'https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80',
-    area: '台北',
-    description:
-      '乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感',
-    group: 99,
-    price: 240,
-    rate: 2,
-  },
-  {
-    id: 2,
-    name: '台中谷關溫泉會1日',
-    imgUrl:
-      'https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80',
-    area: '台中',
-    description:
-      '全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。',
-    group: 20,
-    price: 1765,
-    rate: 7,
-  },
-];
 const _content = document.querySelector('.content__cards');
 const _contentNum = document.getElementById('contentNum');
 const _select = document.querySelector('.content__result__select');
 const _addBtn = document.getElementById('addBtn');
+let data = [];
+
+function init() {
+  axios
+    .get(
+      'https://raw.githubusercontent.com/hexschool/js-training/main/1travelApi.json'
+    )
+    .then(function (response) {
+      data = response.data.data;
+      createList();
+    })
+    .catch(function (error) {
+      alert('遠端資料異常');
+      _content.innerHTML = `查無資料`;
+    });
+}
+init();
 
 function createList() {
+  let sortData = sortID(data);
   let str = '';
-  data.forEach((item) => {
+  sortData.forEach((item) => {
     str += createCard(item);
   });
   _content.innerHTML = str;
   _contentNum.textContent = data.length;
 }
 
-function init() {
-  createList();
+function sortID(data) {
+  let result = [...data];
+  result.sort(function (a, b) {
+    return b.id - a.id;
+  });
+  return result;
 }
-init();
 
 function createCard(item) {
   const content = `<li class="card">
@@ -79,16 +63,15 @@ function selectArea() {
   const area = _select.value;
   let str = '';
   let newData = [];
-
   if (area === '地區搜尋') {
     newData = data;
   } else {
     newData = data.filter((item) => item.area === area);
   }
-  newData.forEach((item) => {
+  let sortData = sortID(newData);
+  sortData.forEach((item) => {
     str += createCard(item);
   });
-
   _content.innerHTML = str;
   _contentNum.textContent = newData.length;
 }
@@ -96,7 +79,12 @@ _select.addEventListener('change', selectArea);
 
 function addNewTicket() {
   const formEl = document.getElementById('addTicket-form');
-  const id = data[data.length - 1].id + 1;
+  let id = '';
+  if (data.length === 0) {
+    id = 0;
+  } else {
+    id = data[data.length - 1].id + 1;
+  }
   const name = document.getElementById('ticketName').value;
   const imgUrl = document.getElementById('ticketImg').value;
   const area = document.getElementById('ticketArea').value;
